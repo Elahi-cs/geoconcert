94881aaeb593c1d43c5e91fec916f3ef2a015f9b
Elahi-cs <elijahshellsanchez@outlook.com>
2021-07-08 19:00:28 +0200

    Initial setup and configuration

---
+++ geoconcert/__init__.py
@@ -0,0 +1,47 @@
+import os
+
+from flask import Flask, redirect, request
+
+import config
+
+def create_app(test_config=None):
+    """
+    This is an application factory. Any configuration, registration, and other
+    setup the app needs will happen inside this function, then the application
+    will be returned.
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config.DevConfig)
+    
+    if test_config is not None:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.before_request
+    def before_request():
+        """Force HTTPS on Heroku."""
+        # https://stackoverflow.com/questions/8436666/ ...
+        # ... how-to-make-python-on-heroku-https-only
+        # TODO: Check for a more permanent solution that works for all providers
+
+        if 'DYNO' in os.environ:
+            if request.url.startswith('http://'):
+                url = request.url.replace('http://', 'https://', 1)
+                code = 301
+                return redirect(url, code=code)
+
+    # a simple page that says hello, used to test app factory
+    @app.route('/hello')
+    def hello():
+        return 'Hello, Flask.'
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
