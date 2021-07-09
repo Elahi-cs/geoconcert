77a921e2c2681a1e34790889a3b8a23463228592
Elahi-cs <elijahshellsanchez@outlook.com>
2021-07-09 11:10:23 +0200

    Auth blueprint done, pending templates

--- geoconcert/auth.py
+++ geoconcert/auth.py
@@ -15,9 +15,6 @@ caches_folder = './.spotify_caches/'
 if not os.path.exists(caches_folder):
     os.makedirs(caches_folder)
 
-def get_user_cache():
-    return caches_folder + session.get('uuid')
-
 @bp.route('/login')
 def login():
     """
@@ -29,11 +26,68 @@ def login():
     if not session.get("uuid"):
         session['uuid'] = str(uuid.uuid4())
 
-    cache_handler = spotipy.cache_handler.CacheFileHandler(
-        cache_path=get_user_cache()
+    cache_handler = get_cache_file_handler(get_user_cache())
+
+    auth_manager = get_auth_manager(cache_handler)
+
+    if request.args.get("code"):
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect(url_for("index"))
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        auth_url = auth_manager.get_authorize_url()
+        return render_template("auth/login.html", auth_url=auth_url)
+
+    spotify_client = spotipy.Spotify(auth_manager=auth_manager)
+
+    try:
+        user = spotify_client.me()
+    except spotipy.exceptions.SpotifyException:
+        error = "Login failed."
+        return redirect(url_for("logout"))
+
+    username = user["display_name"]
+    session["logged_in"] = True
+
+    if error:
+        flash(error)
+    else:
+        flash(f"Welcome, {username}.")
+
+    return redirect(url_for("index"))
+
+@bp.route('/logout')
+def logout():
+    """
+    Delete cache, clean session.
+    """
+    try:
+        os.remove(get_user_cache())
+        session.clear()
+    except OSError as error:
+        print("Error: %s - %s." % (error.filename, error.strerror))
+        flash("Error signing out, please try again.")
+
+    return redirect(url_for("index"))
+
+def get_user_cache():
+    return caches_folder + session.get('uuid')
+
+def get_cache_file_handler(cache_path):
+    spotipy.cache_handler.CacheFileHandler(
+        cache_path=cache_path
         )
 
-    auth_manager = spotipy.oauth2.SpotifyOAuth(
+def get_auth_manager(cache_handler):
+    """
+    Returns a SpotifyOAuth object that implements Authorization Code Flow for 
+    Spotify's OAuth implementation.
+
+    This function exists because SpotifyOAuth objects store token info and so
+    reusing a token could risk a leak of user info, so a new object needs to be
+    created each time.
+    """
+    return spotipy.oauth2.SpotifyOAuth(
         client_id=current_app.config["CLIENT_ID"],
         client_secret=current_app.config["CLIENT_SECRET"],
         redirect_uri=current_app.config["REDIRECT_URI"],
@@ -42,7 +96,12 @@ def login():
         show_dialog=True
     )
 
-    if request.args.get("code"):
-        auth_manager.get_access_token(request.args.get("code"))
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if not session.get("logged_in"):
+            return redirect(url_for("auth.login"))
 
+        return view(**kwargs)
 
+    return wrapped_view
